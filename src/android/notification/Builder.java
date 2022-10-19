6493005076e75cae73fc7fc7229efdc6b97a63ae
Francis Filion <francis@1streporting.com>
2022-10-19 06:39:21 -0400

    Fixed previous commit

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -402,7 +402,7 @@ public final class Builder {
         int reqCode = random.nextInt();
 
         PendingIntent contentIntent = PendingIntent.getService(
-                context, reqCode, intent, FLAG_UPDATE_CURRENT);
+                context, reqCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         builder.setContentIntent(contentIntent);
     }
